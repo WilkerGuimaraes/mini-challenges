f56c91071be63b918bcb833b1d3668ad2f8fea99
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-05-08 14:01:48 -0300

    feat: add light states setup

--- traffic-light/src/app.jsx
+++ traffic-light/src/app.jsx
@@ -1,3 +1,46 @@
+import { useState } from "react";
+
+import "./styles/app.styles.scss";
+
 export function App() {
-  return <h1>Hello World!</h1>;
+  const [redLight, setRedLight] = useState(false);
+  const [yellowLight, setYellowLight] = useState(false);
+  const [greenLight, setGreenLight] = useState(false);
+
+  function activeRedLight() {
+    setRedLight(!redLight);
+    setYellowLight(false);
+    setGreenLight(false);
+  }
+
+  function activeYellowLight() {
+    setRedLight(false);
+    setYellowLight(!yellowLight);
+    setGreenLight(false);
+  }
+
+  function activeGreenLight() {
+    setRedLight(false);
+    setYellowLight(false);
+    setGreenLight(!greenLight);
+  }
+
+  return (
+    <div className="container">
+      <div className="content">
+        <div
+          onClick={activeRedLight}
+          className={`light red ${redLight ? "active" : ""}`}
+        />
+        <div
+          onClick={activeYellowLight}
+          className={`light yellow ${yellowLight ? "active" : ""}`}
+        />
+        <div
+          onClick={activeGreenLight}
+          className={`light green ${greenLight ? "active" : ""}`}
+        />
+      </div>
+    </div>
+  );
 }
