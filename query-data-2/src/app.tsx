c215319995b8e21b646630ea0dca4d34c80b324d
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-05-16 14:57:48 -0300

    (query-data-2) feat: add setup to to fetch and display comments from API json-placeholder

--- query-data-2/src/app.tsx
+++ query-data-2/src/app.tsx
@@ -1,3 +1,54 @@
+import { useQuery } from "@tanstack/react-query";
+import { Loader2 } from "lucide-react";
+
+interface CommentsProps {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
 export function App() {
-  return <h1>Hello World!</h1>;
+  async function fetchComments() {
+    const url = "https://jsonplaceholder.typicode.com/comments";
+
+    const data = fetch(url).then((response) => response.json());
+
+    // delay 1.5s
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
+    return data;
+  }
+
+  const { data: commentsResponse, isLoading } = useQuery<CommentsProps[]>({
+    queryKey: ["get-comments"],
+    queryFn: fetchComments,
+  });
+
+  return (
+    <main className="max-w-5xl w-full my-6 mx-auto">
+      {isLoading && (
+        <span className="inline-flex font-bold text-xl">
+          <Loader2 className="size-8 animate-spin mr-2" />
+          Carregando...
+        </span>
+      )}
+
+      <ul className="space-y-6">
+        {commentsResponse?.map((comment) => (
+          <li key={comment.id}>
+            <h1 className="font-bold text-xl">
+              Name: <span className="font-normal text-sm">{comment.name}</span>
+            </h1>
+            <h1 className="font-bold text-xl">
+              E-mail:{" "}
+              <span className="font-normal text-sm">{comment.email}</span>
+            </h1>
+            <p>{comment.body}</p>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
