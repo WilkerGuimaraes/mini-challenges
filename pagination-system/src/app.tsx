9dc29f336c0b1ee865b841bcb5d7aa92dc77a9fc
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-05-17 21:04:19 -0300

    (pagination-system) feat: add initial setup

---
+++ pagination-system/src/app.tsx
@@ -0,0 +1,49 @@
+import { useQuery } from "@tanstack/react-query";
+import { Loader2 } from "lucide-react";
+
+interface PostsProps {
+  userId: number;
+  id: string;
+  title: string;
+  body: string;
+}
+
+export function App() {
+  const { data: postsResponse, isLoading } = useQuery<PostsProps[]>({
+    queryKey: ["get-posts"],
+    queryFn: async () => {
+      const data = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      ).then((response) => response.json());
+
+      // delay 1.5s
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+
+      return data;
+    },
+  });
+
+  return (
+    <div className="max-w-5xl my-6 mx-auto space-y-6">
+      {isLoading && (
+        <span className="inline-flex gap-2 font-bold text-2xl">
+          <Loader2 className="size-8 animate-spin" />
+          Carregando...
+        </span>
+      )}
+
+      <main className="bg-zinc-800 rounded p-4">
+        <ul className="space-y-6">
+          {postsResponse?.map((post) => (
+            <li key={post.id}>
+              <h1 className="font-bold text-2xl">
+                {post.id} - {post.title}
+              </h1>
+              <p>{post.body}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
