f27f19f4965d57cb10f7364fe96fe8f826227fd0
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-05-18 15:51:25 -0300

    (query-mutation) feat: add user query rendering

--- query-mutation/src/app.tsx
+++ query-mutation/src/app.tsx
@@ -1,3 +1,44 @@
+import { useQuery } from "@tanstack/react-query";
+import { Loader2 } from "lucide-react";
+
+interface UsersProps {
+  id: string;
+  name: string;
+  email: string;
+}
+
 export function App() {
-  return <h1>Hello World!</h1>;
+  const { data: usersResponse, isLoading } = useQuery<UsersProps[]>({
+    queryKey: ["get-users"],
+    queryFn: async () => {
+      const data = await fetch("http://localhost:3333/users").then((response) =>
+        response.json()
+      );
+
+      // delay 1.5s
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+
+      return data;
+    },
+  });
+
+  return (
+    <main className="flex flex-col items-center gap-6 max-w-6xl h-screen mx-auto">
+      {isLoading && (
+        <span className="inline-flex gap-2 font-bold text-2xl">
+          <Loader2 className="size-8 animate-spin" />
+          Carregando...
+        </span>
+      )}
+
+      <ul className="space-y-6">
+        {usersResponse?.map((user) => (
+          <li key={user.id}>
+            <h1 className="font-bold text-2xl">{user.name}</h1>
+            <p className="text-zinc-500">{user.email}</p>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
