4af2103dc6557e8eb7fa28519e509b7a03bbca87
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-05-23 20:02:01 -0300

    (render-filter) feat: add rendering json-server data

--- render-filter/src/app.tsx
+++ render-filter/src/app.tsx
@@ -1,3 +1,52 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { Root } from "./types/users.types";
+
 export function App() {
-  return <h1>Hello World!</h1>;
+  const { data: usersResponse } = useQuery<Root>({
+    queryKey: ["get-users"],
+    queryFn: async () => {
+      const data = await fetch(
+        "http://localhost:3333/users?_page=1&_per_page=30"
+      ).then((response) => response.json());
+
+      return data;
+    },
+  });
+
+  return (
+    <main className="flex max-w-7xl mx-auto">
+      <div className="flex-1 p-4">
+        <table className="w-full text-center">
+          <thead className="font-bold text-2xl">
+            <tr>
+              <th>Id</th>
+              <th>Nome</th>
+              <th>Ativado</th>
+              <th>Hobbies</th>
+              <th>Cidade</th>
+            </tr>
+          </thead>
+
+          {usersResponse?.data.map((user) => (
+            <tbody key={user.id} className="text-xl">
+              <tr>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td
+                  className={
+                    user.isActive ? "text-emerald-500" : "text-red-500"
+                  }
+                >
+                  {String(user.isActive)}
+                </td>
+                <td>{user.hobbies.join(" e ")}</td>
+                <td>{user.address.city}</td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      </div>
+    </main>
+  );
 }
