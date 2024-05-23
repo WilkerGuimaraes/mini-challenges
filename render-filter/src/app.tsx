7308cc4cc30d8473e60814a4eeffac377d0d5055
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-05-23 20:25:30 -0300

    (render-filter) feat: add rendering filter form

--- render-filter/src/app.tsx
+++ render-filter/src/app.tsx
@@ -1,6 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
+import { ChangeEvent, useState } from "react";
 
 import { Root } from "./types/users.types";
+
+interface FilterProps {
+  isActive: boolean | null;
+  hobby: string;
+  city: string;
+}
 
 export function App() {
   const { data: usersResponse } = useQuery<Root>({
@@ -14,8 +21,141 @@ export function App() {
     },
   });
 
+  const [filter, setFilter] = useState<FilterProps>({
+    isActive: null,
+    hobby: "",
+    city: "",
+  });
+
+  const [tempFilter, setTempFilter] = useState<FilterProps>({
+    isActive: null,
+    hobby: "",
+    city: "",
+  });
+
+  function handleTempFilterChange(event: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+
+    if (name === "isActive") {
+      setTempFilter({
+        isActive: value === "true",
+        hobby: "",
+        city: "",
+      });
+    } else if (name === "hobby") {
+      setTempFilter({
+        isActive: null,
+        hobby: value,
+        city: "",
+      });
+    } else if (name === "city") {
+      setTempFilter({
+        isActive: null,
+        hobby: "",
+        city: value,
+      });
+    }
+  }
+
+  function handleFilterChange(event: ChangeEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setFilter(tempFilter);
+  }
+
+  function handleClearFilters() {
+    setFilter({
+      isActive: null,
+      hobby: "",
+      city: "",
+    });
+
+    setTempFilter({
+      isActive: null,
+      hobby: "",
+      city: "",
+    });
+  }
+
+  const filteredUsers = usersResponse?.data.filter((user) => {
+    const matchesIsActive =
+      filter.isActive === null || user.isActive === filter.isActive;
+    const matchesHobby = !filter.hobby || user.hobbies.includes(filter.hobby);
+    const matchesCity = !filter.city || user.address.city === filter.city;
+
+    return matchesIsActive && matchesHobby && matchesCity;
+  });
+
+  const isFilteredUsers =
+    filter.isActive !== null || filter.hobby !== "" || filter.city !== "";
+
   return (
-    <main className="flex max-w-7xl mx-auto">
+    <main className="flex max-w-7xl h-screen mx-auto bg-zinc-800">
+      <div className="flex justify-center items-center max-w-sm w-full bg-zinc-600">
+        <form onSubmit={handleFilterChange} className="flex flex-col gap-4">
+          <div className="inline-flex gap-1">
+            <input
+              type="radio"
+              name="isActive"
+              id="active"
+              value={"true"}
+              checked={tempFilter.isActive === true}
+              onChange={handleTempFilterChange}
+            />
+            <label htmlFor="active">Ativado</label>
+          </div>
+
+          <div className="inline-flex gap-1">
+            <input
+              type="radio"
+              name="isActive"
+              id="desactive"
+              value={"false"}
+              checked={tempFilter.isActive === false}
+              onChange={handleTempFilterChange}
+            />
+            <label htmlFor="desactive">Desativado</label>
+          </div>
+
+          <div className="inline-flex gap-1">
+            <input
+              type="radio"
+              name="hobby"
+              id="hobby"
+              value={"Cooking"}
+              checked={tempFilter.hobby === "Cooking"}
+              onChange={handleTempFilterChange}
+            />
+            <label htmlFor="hobby">Cooking</label>
+          </div>
+
+          <div className="inline-flex gap-1">
+            <input
+              type="radio"
+              name="city"
+              id="city"
+              value={"Mapleton"}
+              checked={tempFilter.city === "Mapleton"}
+              onChange={handleTempFilterChange}
+            />
+            <label htmlFor="city">Mapleton</label>
+          </div>
+
+          <button
+            type="submit"
+            className="h-10 w-20 font-bold rounded bg-emerald-600"
+          >
+            Filtrar
+          </button>
+          <button
+            type="button"
+            onClick={handleClearFilters}
+            className="h-10 w-20 font-bold rounded bg-slate-600"
+          >
+            Limpar
+          </button>
+        </form>
+      </div>
+
       <div className="flex-1 p-4">
         <table className="w-full text-center">
           <thead className="font-bold text-2xl">
@@ -28,23 +168,24 @@ export function App() {
             </tr>
           </thead>
 
-          {usersResponse?.data.map((user) => (
-            <tbody key={user.id} className="text-xl">
-              <tr>
-                <td>{user.id}</td>
-                <td>{user.name}</td>
-                <td
-                  className={
-                    user.isActive ? "text-emerald-500" : "text-red-500"
-                  }
-                >
-                  {String(user.isActive)}
-                </td>
-                <td>{user.hobbies.join(" e ")}</td>
-                <td>{user.address.city}</td>
-              </tr>
-            </tbody>
-          ))}
+          {isFilteredUsers &&
+            filteredUsers?.map((user) => (
+              <tbody key={user.id} className="text-xl">
+                <tr>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td
+                    className={
+                      user.isActive ? "text-emerald-500" : "text-red-500"
+                    }
+                  >
+                    {String(user.isActive)}
+                  </td>
+                  <td>{user.hobbies.join(" e ")}</td>
+                  <td>{user.address.city}</td>
+                </tr>
+              </tbody>
+            ))}
         </table>
       </div>
     </main>
