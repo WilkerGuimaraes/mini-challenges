9c12b7134cb97514c5d3e2e40cf67ea7e9a227a5
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-08-07 16:36:18 -0300

    (query-data) test: render form inputs, buttons and users data

--- query-data/src/app.spec.tsx
+++ query-data/src/app.spec.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { describe, it, expect } from "vitest";
 
 import { App } from "./app";
 
@@ -13,13 +14,41 @@ describe("Task Component test", () => {
       </QueryClientProvider>
     );
 
-    expect(screen.getByLabelText(/Usuário 1/i));
-    expect(screen.getByLabelText(/Usuário 2/i));
-    expect(screen.getByLabelText(/Usuário 3/i));
-    expect(screen.getByLabelText(/Usuário 4/i));
-    expect(screen.getByLabelText(/Usuário 5/i));
+    expect(screen.getByLabelText(/Usuário 1/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Usuário 2/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Usuário 3/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Usuário 4/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Usuário 5/i)).toBeInTheDocument();
 
-    expect(screen.getByText(/Filtrar/i));
-    expect(screen.getByText(/Limpar/i));
+    expect(screen.getByText(/Filtrar/i)).toBeInTheDocument();
+    expect(screen.getByText(/Limpar/i)).toBeInTheDocument();
+  });
+
+  it("renders all data of all users", async () => {
+    render(
+      <QueryClientProvider client={queryCLient}>
+        <App />
+      </QueryClientProvider>
+    );
+
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
+    expect(
+      await screen.findByText(
+        /sunt aut facere repellat provident occaecati excepturi optio reprehenderit/i
+      )
+    ).toBeInTheDocument();
+    expect(
+      await screen.findByText(/et ea vero quia laudantium autem/i)
+    ).toBeInTheDocument();
+    expect(
+      await screen.findByText(
+        /asperiores ea ipsam voluptatibus modi minima quia sint/i
+      )
+    ).toBeInTheDocument();
+    expect(
+      await screen.findByText(/ullam ut quidem id aut vel consequuntur/i)
+    ).toBeInTheDocument();
+    expect(await screen.findByText(/non est facere/i)).toBeInTheDocument();
   });
 });
