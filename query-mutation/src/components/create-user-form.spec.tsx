517788d9cc3c331ada0d4994c9fba0afdea61cd1
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-08-08 16:21:15 -0300

    (query-mutation) test: render form validation errors

--- query-mutation/src/components/create-user-form.spec.tsx
+++ query-mutation/src/components/create-user-form.spec.tsx
@@ -1,7 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import { CreateUserForm } from "./create-user-form";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { CreateUserForm } from "./create-user-form";
 
 const queryClient = new QueryClient();
 
@@ -16,5 +17,21 @@ describe("CreateUserForm testing", () => {
     expect(screen.getByLabelText(/Nome:/i));
     expect(screen.getByLabelText(/E-mail:/i));
     expect(screen.getByText(/Enviar/i));
+  });
+
+  it("should render form validation errors", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <CreateUserForm />
+      </QueryClientProvider>,
+    );
+
+    const sendButton = screen.getByText(/Enviar/i);
+    fireEvent.click(sendButton);
+
+    await waitFor(() => {
+      expect(screen.getByText(/O nome é obrigatório./i)).toBeInTheDocument();
+      expect(screen.getByText(/O e-mail é obrigatório./i)).toBeInTheDocument();
+    });
   });
 });
